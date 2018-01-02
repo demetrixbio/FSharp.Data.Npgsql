835979db1abe2088bb5cc59693b265afa46f3443
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2018-01-02 12:03:31 -0800

    WIP

--- src/ISqlCommand.fs
+++ src/ISqlCommand.fs
@@ -1,5 +1,16 @@
 ﻿namespace FSharp.Data
 
+///<summary>Enum describing output type</summary>
+type ResultType =
+///<summary>Sequence of custom records with properties matching column names and types</summary>
+    | Records = 0
+///<summary>Sequence of tuples matching column types with the same order</summary>
+    | Tuples = 1
+///<summary>Typed DataTable <see cref='T:FSharp.Data.DataTable`1'/></summary>
+    | DataTable = 2
+///<summary>raw DataReader</summary>
+    | DataReader = 3
+
 open System
 open System.Data
 open Npgsql
